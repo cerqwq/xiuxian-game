73261a8dbbaeea9a7e53434ad46888e1bdec0411
sans <995935899@qq.com>
2026-06-07 04:36:04 +0800

    Initial commit

---
+++ start.bat
@@ -0,0 +1,9 @@
+@echo off
+echo.
+echo ========================================
+echo   修仙游戏 - 独立服务器
+echo ========================================
+echo.
+cd /d "%~dp0\.."
+python xiuxian\app.py
+pause
