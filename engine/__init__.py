73a270eff896a4cbc41263c3e40a9994f694cd6e
sans <995935899@qq.com>
2026-06-07 04:36:13 +0800

    Initial commit

---
+++ engine/__init__.py
@@ -0,0 +1,27 @@
+"""
+鬼谷八荒风格 · 文字修仙游戏引擎
+模块化版本
+"""
+
+# 从各个子模块导入所有公共接口
+from .realms import *
+from .elements import *
+from .items import *
+from .crafting import *
+from .combat import *
+from .exploration import *
+from .npc import *
+from .quests import *
+from .achievements import *
+from .techniques import *
+from .abilities import *
+from .sects import *
+from .pets import *
+from .dungeons import *
+from .world_boss import *
+from .equipment import *
+from .alchemy import *
+from .auction import *
+from .character import *
+from .cultivation import *
+from .rebirth import *
